1291f01bbd044e84688af2676ea6b74172e81398
aled1027 <a.led1027@gmail.com>
2015-04-17 22:59:41 -0700

    initial commit

---
+++ programming/readme.md
@@ -0,0 +1,2 @@
+# Notes to Jacob
+- remove datasets from library? Is that standard practice?
---
+++ programming/schelling/main.py
@@ -0,0 +1,106 @@
+class SchellingCA:
+    is_done = False
+    amount_segregation = 0
+
+    def __init__(self, width=8, height=8, races=['white','black']):
+        self.width = width
+        self.height = height
+        self.races = races
+
+        preferences = {'white' : (0,1), 'black' : (0,1)}
+
+        self.state = []
+        for i in range(self.width):
+            li = []
+            for j in range(self.height):
+                li.append(Person(preferences=preferences))
+            self.state.append(li)
+
+
+    def iterate(self):
+        could_move = self.move_someone()
+        if (could_move == False):
+            self.is_done = True
+    def move_someone(self):
+        # find someone with who is unhappy.
+        self.updatePeoplesStates()
+        canMoveSomeone = True
+        return canMoveSomeone
+
+    def get_neighbors(self,i,j):
+        if i < 0:
+             raise RuntimeError("Width %d Lower than number of cells. " %i)
+        elif i >= self.width:
+             raise RuntimeError("Width %d Lower than number of cells. " %i)
+        elif j < 0:
+             raise RuntimeError("Height %d Higher than number of cells. " %i)
+        elif j >= self.height:
+             raise RuntimeError("Height %d Higher than number of cells. " %i)
+
+        deltas = [(-1,0), (1,0), (0,-1), (0,1)]
+        nbr_coords =  [(i+a, j+b) for (a,b) in deltas if (-1 < i+a < self.width) and (-1 < j+b < self.height)]
+        return [self.state[x][y] for (x,y) in nbr_coords]
+
+    def iterate(self):
+        for i in range(self.width):
+            for j in range(self.height):
+                nbr_races = [nbr.race for nbr in self.get_neighbors(i,j)]
+                nbr_dict = {r: nbr_races.count(r) for r in self.races}
+                self.state[i][j].update_happiness(nbr_dict)
+
+
+
+
+
+
+
+
+
+
+    def get_amount_of_segregation():
+        pass
+
+
+class Person:
+    # prefernces of the form: {'white': (.2,.3)} indicates prefers more than 20% white neighbors, less than %30 white neighbors.
+
+    def __init__(self, preferences, race='white', is_happy=False, races=['white','black']):
+        self.race = race
+        self.races = races
+        self.is_happy = is_happy
+        self.preferences = preferences
+
+    def __str__(self):
+        return "(Race: "+self.race + ", is_happy:" + str(self.is_happy) + ", preferences:" + str(self.preferences) + ")"
+    def __repr__(self):
+        return str(self)
+
+    def set_happiness(self,is_happy):
+        self.is_happy = is_happy
+
+    def update_happiness(self,nbr_dict):
+        num_nbrs = sum(nbr_dict.values())
+
+        for race in self.races:
+            pct_race = nbr_dict[race] / num_nbrs
+            lower_bound, upper_bound = self.preferences[race]
+
+            print(lower_bound, pct_race, upper_bound)
+            if lower_bound <= pct_race <= upper_bound:
+                pass
+            else:
+                # failed a condition. we are so unhappy
+                self.is_unhappy = False
+                return False
+
+        # passed all of the conditions, so we are happy.
+        self.is_unhappy = True
+        return True
+
+
+
+
+s = SchellingCA(width=8,height=8)
+
+
+
